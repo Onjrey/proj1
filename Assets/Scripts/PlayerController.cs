abe3626bd9bc2c280b81def6a0908f32d9a78dc2
Onjrey <ducha5757@gmail.com>
2024-11-13 19:30:37 +0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed = 20;
+    void Update()
+    {
+        transform.Translate(new Vector3(0,0,1) * Time.deltaTime * speed);
+    }
+}
